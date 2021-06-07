88e020c8142e531b0ba41508d6795dfa9654dd58
cperez <chrper31@gmail.com>
2021-06-07 16:38:52 +0200

    add activity_profil_neighbour.xml (avec création de l'image de l'ativité du profil du neighbour)

---
+++ app/src/main/java/com/openclassrooms/entrevoisins/ui/neighbour_list/ProfilNeighbourActivity.java
@@ -0,0 +1,22 @@
+package com.openclassrooms.entrevoisins.ui.neighbour_list;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.openclassrooms.entrevoisins.R;
+import com.openclassrooms.entrevoisins.model.Neighbour;
+
+public class ProfilNeighbourActivity extends AppCompatActivity {
+
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profil_neighbour);
+
+
+
+    }
+}
